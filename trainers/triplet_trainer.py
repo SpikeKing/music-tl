3e818afa505a67f304bb44532da13af2564d4098
wcl1 <wcl1@meitu.com>
2018-05-04 13:24:01 +0800

    update

--- trainers/triplet_trainer.py
+++ trainers/triplet_trainer.py
@@ -66,8 +66,8 @@ class TripletTrainer(TrainerBase):
 
         x_train = x_train[:19 * 100]
         y_train = y_train[:19 * 100]
-        x_test = x_test[19 * 10:19 * 20]
-        y_test = y_test[19 * 10:19 * 20]
+        x_test = x_train[19 * 100:19 * 120]
+        y_test = x_train[19 * 100:19 * 120]
 
         self.train_core(x_train, y_train, x_test, y_test)
 
