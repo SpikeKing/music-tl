77125dc82934a8c22f5d235d96d66768e319def4
wcl1 <wcl1@meitu.com>
2018-05-07 15:09:14 +0800

    update

--- data_loaders/data_augment.py
+++ data_loaders/data_augment.py
@@ -3,195 +3,161 @@
 """
 Copyright (c) 2018. All rights reserved.
 Created by C. L. Wang on 2018/5/2
+
+音频数据增强（Data Augment）
 """
+
+import os
+import sys
 
 import cv2
 import librosa
 import numpy as np
-import os
-import sys
 
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if p not in sys.path:
     sys.path.append(p)
 
-from multiprocessing.pool import ThreadPool, Pool
+from multiprocessing.pool import Pool
 from pyAudioAnalysis import audioFeatureExtraction
 from root_dir import ROOT_DIR
 from utils.utils import *
 
 
 def audio_slice(y, sr, name_id, folder):
-    # name = file_path.split('/')[-1]
-    # folder = file_path.replace(name, '')
-    # print '【INFO] 文件名: %s' % name
-    # print '【INFO] 文件夹: %s' % folder
-    # y, sr = librosa.load(file_path)  # 读取音频
+    """
+    音频剪裁，平分5段，剪裁分为三种，[0:3]，[1:4]，[2:5]
+    """
     n_part = len(y) / 5
-    # file1 = os.path.join(folder, name + '.slice_first.mp3')
-    # wavfile.write(file1, sr, y[:n_part * 3])  # 写入音频
+
     file1 = os.path.join(folder, name_id + '.slice_first' + '.npy')
     np.save(file1, get_feature(y[:n_part * 3], sr))
 
-    # file2 = os.path.join(folder, name + '.slice_middle.mp3')
-    # wavfile.write(file2, sr, y[n_part * 1:n_part * 4])  # 写入音频
     file2 = os.path.join(folder, name_id + '.slice_middle' + '.npy')
     np.save(file2, get_feature(y[n_part * 1:n_part * 4], sr))
 
-    # file3 = os.path.join(folder, name + '.slice_last.mp3')
-    # wavfile.write(file3, sr, y[n_part * 2:])  # 写入音频
     file3 = os.path.join(folder, name_id + '.slice_last' + '.npy')
     np.save(file3, get_feature(y[n_part * 2:], sr))
 
 
 def audio_roll(y, sr, name_id, folder):
-    # name = file_path.split('/')[-1]
-    # folder = file_path.replace(name, '')
-    # print '【INFO] 文件名: %s' % name
-    # print '【INFO] 文件夹: %s' % folder
-    # y, sr = librosa.load(file_path)  # 读取音频
-
+    """
+    音频旋转，平分4段，旋转分为三种，[3, 0, 1, 2]，[2, 3, 0, 1]，[1, 2, 3, 0]
+    """
     n_part = len(y) / 4
 
-    # file1 = os.path.join(folder, name + '.roll_1.mp3')
-    # wavfile.write(file1, sr, np.roll(y, n_part))  # 写入音频
     file1 = os.path.join(folder, name_id + '.roll_1' + '.npy')
     np.save(file1, get_feature(np.roll(y, n_part), sr))
 
-    # file2 = os.path.join(folder, name + '.roll_2.mp3')
-    # wavfile.write(file2, sr, np.roll(y, n_part * 2))  # 写入音频
     file2 = os.path.join(folder, name_id + '.roll_2' + '.npy')
     np.save(file2, get_feature(np.roll(y, n_part * 2), sr))
 
-    # file3 = os.path.join(folder, name + '.roll_3.mp3')
-    # wavfile.write(file3, sr, np.roll(y, n_part * 3))  # 写入音频
     file3 = os.path.join(folder, name_id + '.roll_3' + '.npy')
     np.save(file3, get_feature(np.roll(y, n_part * 3), sr))
 
 
 def audio_tune(y, sr, name_id, folder):
-    # name = file_path.split('/')[-1]
-    # folder = file_path.replace(name, '')
-    # print '【INFO] 文件名: %s' % name
-    # print '【INFO] 文件夹: %s' % folder
-    # y, sr = librosa.load(file_path)  # 读取音频
-
+    """
+    音频调音，调音分为三种，拉长为5%，10%，15%
+    """
     tune1 = cv2.resize(y, (1, int(len(y) * 1.05))).squeeze()
-    # file1 = os.path.join(folder, name + '.tune_5.mp3')
-    # wavfile.write(file1, sr, y_tune)  # 写入音频
     file1 = os.path.join(folder, name_id + '.tune_5' + '.npy')
     np.save(file1, get_feature(tune1, sr))
 
     tune2 = cv2.resize(y, (1, int(len(y) * 1.10))).squeeze()
-    # file2 = os.path.join(folder, name + '.tune_10.mp3')
-    # wavfile.write(file2, sr, y_tune)  # 写入音频
     file2 = os.path.join(folder, name_id + '.tune_10' + '.npy')
     np.save(file2, get_feature(tune2, sr))
 
     tune3 = cv2.resize(y, (1, int(len(y) * 1.15))).squeeze()
-    # file3 = os.path.join(folder, name + '.tune_15.mp3')
-    # wavfile.write(file3, sr, tune1)  # 写入音频
     file3 = os.path.join(folder, name_id + '.tune_15' + '.npy')
     np.save(file3, get_feature(tune3, sr))
 
 
 def audio_noise(y, sr, name_id, folder):
-    # name = file_path.split('/')[-1]
-    # folder = file_path.replace(name, '')
-    # print '【INFO] 文件名: %s' % name
-    # print '【INFO] 文件夹: %s' % folder
-    # y, sr = librosa.load(file_path)  # 读取音频
-
+    """
+    音频噪声，添加高斯噪声，噪声分为三种，添加1%，2%，3%
+    """
     np.random.seed(seed=47)
     wn = np.random.randn(len(y))
 
     yn1 = np.where(y != 0.0, y + 0.01 * wn, 0.0)
-    # file1 = os.path.join(folder, name + '.noise_1.mp3')
-    # wavfile.write(file1, sr, yn1)  # 写入音频
     file1 = os.path.join(folder, name_id + '.noise_1' + '.npy')
     np.save(file1, get_feature(yn1, sr))
 
     yn2 = np.where(y != 0.0, y + 0.02 * wn, 0.0)
-    # file2 = os.path.join(folder, name + '.noise_2.mp3')
-    # wavfile.write(file2, sr, yn2)  # 写入音频
     file2 = os.path.join(folder, name_id + '.noise_2' + '.npy')
     np.save(file2, get_feature(yn2, sr))
 
     yn3 = np.where(y != 0.0, y + 0.03 * wn, 0.0)
-    # file3 = os.path.join(folder, name + '.noise_3.mp3')
-    # wavfile.write(file3, sr, yn3)  # 写入音频
     file3 = os.path.join(folder, name_id + '.noise_3' + '.npy')
     np.save(file3, get_feature(yn3, sr))
 
 
 def audio_high(y, sr, name_id, folder):
-    # name = file_path.split('/')[-1]
-    # folder = file_path.replace(name, '')
-    # print '【INFO] 文件名: %s' % name
-    # print '【INFO] 文件夹: %s' % folder
-    # y, sr = librosa.load(file_path)  # 读取音频
-
+    """
+    音频高音，提高音调，高音分为三种，添加5%，10%，15%
+    """
     yn1 = np.where(y != 0.0, y * 1.05, 0.0)
-    # file1 = os.path.join(folder, name + '.high_5.mp3')
-    # wavfile.write(file1, sr, yn1)  # 写入音频
     file1 = os.path.join(folder, name_id + '.high_5' + '.npy')
     np.save(file1, get_feature(yn1, sr))
 
     yn2 = np.where(y != 0.0, y * 1.10, 0.0)
-    # file2 = os.path.join(folder, name + '.high_10.mp3')
-    # wavfile.write(file2, sr, yn2)  # 写入音频
     file2 = os.path.join(folder, name_id + '.high_10' + '.npy')
     np.save(file2, get_feature(yn2, sr))
 
     yn3 = np.where(y != 0.0, y * 1.15, 0.0)
-    # file3 = os.path.join(folder, name + '.high_15.mp3')
-    # wavfile.write(file3, sr, yn3)  # 写入音频
     file3 = os.path.join(folder, name_id + '.high_15' + '.npy')
     np.save(file3, get_feature(yn3, sr))
 
 
 def audio_low(y, sr, name_id, folder):
-    # name = file_path.split('/')[-1]
-    # folder = file_path.replace(name, '')
-    # print '【INFO] 文件名: %s' % name
-    # print '【INFO] 文件夹: %s' % folder
-    # y, sr = librosa.load(file_path)  # 读取音频
-
+    """
+    音频低音，降低音调，低音分为三种，添加-5%，-10%，-15%
+    """
     yn1 = np.where(y != 0.0, y * 0.95, 0.0)
-    # file1 = os.path.join(folder, name + '.low_5.mp3')
-    # wavfile.write(file1, sr, yn1)  # 写入音频
     file1 = os.path.join(folder, name_id + '.low_5' + '.npy')
     np.save(file1, get_feature(yn1, sr))
 
     yn2 = np.where(y != 0.0, y * 0.90, 0.0)
-    # file2 = os.path.join(folder, name + '.low_10.mp3')
-    # wavfile.write(file2, sr, yn2)  # 写入音频
     file2 = os.path.join(folder, name_id + '.low_10' + '.npy')
     np.save(file2, get_feature(yn2, sr))
 
     yn3 = np.where(y != 0.0, y * 0.85, 0.0)
-    # file3 = os.path.join(folder, name + '.low_15.mp3')
-    # wavfile.write(file3, sr, yn3)  # 写入音频
     file3 = os.path.join(folder, name_id + '.low_15' + '.npy')
     np.save(file3, get_feature(yn3, sr))
 
 
-def save_features(params):
+def generate_augment(params):
+    """
+    音频增强
+    :param params: 参数，[文件路径，音频ID，存储文件夹]
+    :return: None
+    """
     file_path, name_id, folder = params
     try:
-        print '[INFO] ' + name_id
+        print '[INFO] 音频ID ' + name_id
         y, sr = librosa.load(file_path)
         saved_path = os.path.join(folder, name_id + '.npy')
-        np.save(saved_path, get_feature(y, sr))
+        np.save(saved_path, get_feature(y, sr))  # 存储原文件的npy
 
+        # 18种数据增强
         audio_slice(y, sr, name_id, folder)
         audio_roll(y, sr, name_id, folder)
         audio_tune(y, sr, name_id, folder)
         audio_noise(y, sr, name_id, folder)
         audio_high(y, sr, name_id, folder)
         audio_low(y, sr, name_id, folder)
+
     except Exception as e:
-        print '[Exception] %s' % e
+        print '[Exception] 异常: %s' % e
+
+
+def mp_augment(n_process=40):
+    """
+    多进程的音频增强
+    :param n_process: 进程数
+    :return:
+    """
 
 
 def generate_npy_data_train(tn=40):
@@ -210,21 +176,9 @@ def generate_npy_data_train(tn=40):
     for path, name in zip(paths, names):
         name_id = name.split('_')[0]
         params = (path, name_id, npy_train)
-        p.apply_async(save_features, args=(params,))
+        p.apply_async(generate_augment, args=(params,))
     p.close()
     p.join()
-
-    # raw_test = os.path.join(ROOT_DIR, 'experiments', 'raw_data', 'test')
-    # paths, names = traverse_dir_files(raw_test)
-    # param_list = []
-    # for path, name in zip(paths, names):
-    #     name_id = name.split('_')[0]
-    #     param_list.append((path, name_id, npy_test))
-    # print "[INFO] 测试数据: %s" % len(param_list)
-    # pool1 = ThreadPool(tn)
-    # pool1.map(save_features, param_list)
-    # pool1.close()
-    # pool1.join()
 
     print "[INFO] 特征提取结束"
 
@@ -245,7 +199,7 @@ def generate_npy_data_fot_test(tn=40):
     for path, name in zip(paths, names):
         name_id = name.split('_')[0]
         params = (path, name_id, npy_test)
-        p.apply_async(save_features, args=(params,))
+        p.apply_async(generate_augment, args=(params,))
     p.close()
     p.join()
 
--- data_loaders/data_spliter.py
+++ data_loaders/data_spliter.py
@@ -3,6 +3,8 @@
 """
 Copyright (c) 2018. All rights reserved.
 Created by C. L. Wang on 2018/5/2
+
+将数据集划分训练和测试
 """
 import os
 import sys
@@ -16,6 +18,13 @@ from utils.utils import *
 
 
 def split_train_test(folder_name, n_part=6):
+    """
+    将数据集分为训练数据和测试数据
+
+    :param folder_name: 文件夹名
+    :param n_part: 划分块，1份为测试，其余为训练
+    :return: None
+    """
     paths, names = traverse_dir_files(folder_name)
     train_dir = os.path.join(folder_name, 'train')
     test_dir = os.path.join(folder_name, 'test')
@@ -27,7 +36,7 @@ def split_train_test(folder_name, n_part=6):
     for path, name in zip(paths, names):
         count += 1
         if count % n_part != 0:
-            shutil.move(path, os.path.join(train_dir, name))
+            shutil.move(path, os.path.join(train_dir, name))  # 移动文件
         else:
             shutil.move(path, os.path.join(test_dir, name))
 
@@ -39,4 +48,4 @@ def split_train_test(folder_name, n_part=6):
 
 if __name__ == '__main__':
     data_dir = os.path.join(ROOT_DIR, 'experiments', 'raw_data')
-    split_train_test(data_dir)
+    split_train_test(data_dir)  # 划分训练和测试
