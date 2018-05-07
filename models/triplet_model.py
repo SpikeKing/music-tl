078af89d6ca7f14db6e59ddc926f25cb8cabfba6
wcl1 <wcl1@meitu.com>
2018-05-07 16:10:55 +0800

    update

--- models/triplet_model.py
+++ models/triplet_model.py
@@ -4,11 +4,10 @@ Copyright (c) 2018. All rights reserved.
 Created by C. L. Wang on 2018/4/18
 """
 import os
-import tensorflow as tf
 
+import tensorflow as tf
 from keras import Input, Model
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, K, Concatenate, Conv1D, \
-    BatchNormalization, MaxPooling1D, LSTM
+from keras.layers import Dropout, K, Concatenate, Conv1D, BatchNormalization, MaxPooling1D, LSTM
 from keras.optimizers import Adam
 from keras.utils import plot_model
 
@@ -27,8 +26,7 @@ class TripletModel(ModelBase):
         self.build_model()
 
     def build_model(self):
-        # self.model = self.triplet_loss_model()  # 使用Triplet Loss训练Model
-        self.model = self.triplet_loss_model_v2()
+        self.model = self.triplet_loss_model()
 
     @staticmethod
     def triplet_loss(y_true, y_pred):
@@ -47,13 +45,15 @@ class TripletModel(ModelBase):
         print "[INFO] model - triplet_loss shape: %s" % str(loss.shape)
         return loss
 
-    def triplet_loss_model_v2(self):
+    def triplet_loss_model(self):
         anc_input = Input(shape=(256, 32), name='anc_input')  # anchor
         pos_input = Input(shape=(256, 32), name='pos_input')  # positive
         neg_input = Input(shape=(256, 32), name='neg_input')  # negative
 
         shared_model = self.deep_conv_lstm()  # 共享模型
 
+        # 必须指定GPU，否则出现异常，参考Keras的Device parallelism
+        # https://keras.io/getting-started/faq/#how-can-i-run-a-keras-model-on-multiple-gpus
         with tf.device('/gpu:0'):
             anc_out = shared_model(anc_input)
         with tf.device('/gpu:1'):
@@ -61,9 +61,9 @@ class TripletModel(ModelBase):
         with tf.device('/gpu:2'):
             neg_out = shared_model(neg_input)
 
-        print "[INFO] model - 锚shape: %s" % str(anc_out.get_shape())
-        print "[INFO] model - 正shape: %s" % str(pos_out.get_shape())
-        print "[INFO] model - 负shape: %s" % str(neg_out.get_shape())
+        print "[INFO] anc_out - 锚shape: %s" % str(anc_out.get_shape())
+        print "[INFO] pos_out - 正shape: %s" % str(pos_out.get_shape())
+        print "[INFO] neg_out - 负shape: %s" % str(neg_out.get_shape())
 
         with tf.device('/cpu:0'):
             output = Concatenate()([anc_out, pos_out, neg_out])  # 连接
@@ -76,6 +76,11 @@ class TripletModel(ModelBase):
         return model
 
     def deep_conv_lstm(self):
+        """
+        共享模型deep_conv_lstm
+        :return: 模型
+        """
+
         def cnn_lstm_cell(cell_input):
             """
             基于DeepConvLSTM算法, 创建子模型
