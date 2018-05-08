6554723115b00ae9ebde97359dbf03383576df3f
wcl1 <wcl1@meitu.com>
2018-05-08 18:19:32 +0800

    添加Hash处理逻辑

---
+++ hash/distance_api.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""
+Copyright (c) 2018. All rights reserved.
+Created by C. L. Wang on 2018/5/8
+"""
+
+import os
+import sys
+import numpy as np
+
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if p not in sys.path:
+    sys.path.append(p)
+
+from datetime import datetime
+from root_dir import ROOT_DIR
+
+from utils.utils import sort_two_list
+
+
+class DistanceApi(object):
+    def __init__(self):
+        self.b_list, self.l_list, self.n_list = self.load_data()
+        pass
+
+    @staticmethod
+    def load_data():
+        file_name = 'data_test_200.bin.npz'
+        data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
+        data_all = np.load(data_path)
+        b_list = data_all['b_list'].astype(np.int64)
+        l_list = data_all['l_list']
+        n_list = data_all['n_list']
+        return b_list, l_list, n_list
+
+    def distance(self, audio_id):
+        # 获取索引ID
+        i_name = audio_id + '.npy'
+        i_id = np.where(self.n_list == i_name)
+        i_id = int(i_id[0])  # 索引ID
+
+        def hamdist_for(data):  # Hamming距离
+            return self.hamdist(self.b_list[i_id], data)
+
+        start_time = datetime.now()  # 起始时间
+        b_list_dist = [hamdist_for(x) for x in np.nditer(self.b_list)]
+        elapsed_time = (datetime.now() - start_time).total_seconds()
+        run_num = self.b_list.shape[0]
+        tps = float(run_num) / float(elapsed_time)
+        print "[INFO] Num: %s, Time: %s s, TPS: %0.0f (%s ms)" % (run_num, elapsed_time, tps, (1 / tps * 1000))
+
+        sb_list, sn_list = sort_two_list(list(b_list_dist), list(self.n_list))
+        return sb_list[0:20], sn_list[0:20]
+
+    @staticmethod
+    def hamdist(a, b):
+        # 较慢
+        # bits=128
+        # x = (a ^ b) & ((1 << bits) - 1)
+        # ans = 0
+        # while x:
+        #     ans += 1
+        #     x &= x - 1
+        # return ans
+        return bin(a ^ b).count('1')  # 更快
+
+
+if __name__ == '__main__':
+    da = DistanceApi()
+    audio_name = '926738397'
+    print('[INFO] 目标音频: %s' % audio_name)
+    rb_list, rn_list = da.distance(audio_name)
+    print('[INFO] 距离: %s' % rb_list)
+    print('[INFO] 相似: %s' % rn_list)
---
+++ hash/hash_preprocessor.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""
+Copyright (c) 2018. All rights reserved.
+Created by C. L. Wang on 2018/5/8
+"""
+import os
+import sys
+import numpy as np
+
+from keras.layers import K
+from keras.models import load_model
+
+p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if p not in sys.path:
+    sys.path.append(p)
+
+from root_dir import ROOT_DIR
+
+
+class HashPreProcessor(object):
+    def __init__(self):
+        pass
+
+    def process(self):
+        print('[INFO] 转换开始')
+        model_path = os.path.join(ROOT_DIR, "experiments/music_tl/checkpoints", "triplet_loss_model_91_0.9989.h5")
+        model = load_model(model_path, custom_objects={'triplet_loss': self.triplet_loss})
+
+        file_name = 'data_test_200.npz'
+        data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
+        data_all = np.load(data_path)
+        X_test = data_all['f_list']
+        l_list = data_all['l_list']
+        n_list = data_all['n_list']
+
+        def split_num(name):
+            name = str(name)
+            return len(name.split('.'))  # 长度为2的音频是原始音频
+
+        # 获取原始音频的索引
+        n_list_r = np.reshape(n_list, (-1, 1))
+        n_list_num = np.apply_along_axis(split_num, axis=1, arr=n_list_r)
+        o_indexes = np.where(n_list_num == 2, True, False)  # 原始音频的索引
+
+        n_list = n_list[o_indexes]
+        l_list = l_list[o_indexes]
+        X_test = X_test[o_indexes]
+
+        print('[INFO] 转换数量: %s' % n_list.shape[0])
+
+        X_test = np.transpose(X_test, [0, 2, 1])
+
+        X = {
+            'anc_input': X_test,
+            'pos_input': np.zeros(X_test.shape),
+            'neg_input': np.zeros(X_test.shape)
+        }
+        res = model.predict(X)
+        data = res[:, :128]
+        oz_arr = np.where(data >= 0.0, 1.0, 0.0).astype(int)
+        # print np.sum(oz_arr, axis=1)  # 测试分布
+        oz_bin = np.apply_along_axis(self.to_binary, axis=1, arr=oz_arr)
+
+        out_path = os.path.join(ROOT_DIR, 'experiments', file_name.replace('.npz', '') + ".bin.npz")
+        np.savez(out_path, b_list=oz_bin, l_list=l_list, n_list=n_list)
+
+        print('[INFO] 输出示例: %s %s %s' % (str(oz_bin.shape), bin(oz_bin[0]), oz_bin[0]))
+        print('[INFO] 转换结束')
+
+    @staticmethod
+    def triplet_loss(y_true, y_pred):
+        """
+        Triplet Loss的损失函数
+        """
+        anc, pos, neg = y_pred[:, 0:128], y_pred[:, 128:256], y_pred[:, 256:]
+
+        # 欧式距离
+        pos_dist = K.sum(K.square(anc - pos), axis=-1, keepdims=True)
+        neg_dist = K.sum(K.square(anc - neg), axis=-1, keepdims=True)
+        basic_loss = pos_dist - neg_dist + 10.0
+
+        loss = K.maximum(basic_loss, 0.0)
+
+        print "[INFO] model - triplet_loss shape: %s" % str(loss.shape)
+        return loss
+
+    @staticmethod
+    def to_binary(bit_list):
+        out = 0
+        for bit in bit_list:
+            out = (out << 1) | bit
+        return out
+
+
+if __name__ == '__main__':
+    hpp = HashPreProcessor()
+    hpp.process()
