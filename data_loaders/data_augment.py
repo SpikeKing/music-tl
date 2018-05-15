e006f7c4f154ef18d7efb4ff500828e4972016d1
wcl1 <wcl1@meitu.com>
2018-05-15 11:13:02 +0800

    update

--- data_loaders/data_augment.py
+++ data_loaders/data_augment.py
@@ -251,6 +251,7 @@ def mp_augment(raw_dir, npy_dir, n_process=40):
     for path, name in zip(paths, names):
         name_id = name.split('_')[0]
         params = (path, name_id, npy_dir)
+        print params
         generate_augment(params)
         p.apply_async(generate_augment, args=(params,))
     # p.close()
