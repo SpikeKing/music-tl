5d6f6b55723942c3baf99c05bf7dff921c638752
wcl1 <wcl1@meitu.com>
2018-05-22 09:38:20 +0800

    update

--- hash/hash_preprocessor_mxnet.py
+++ hash/hash_preprocessor_mxnet.py
@@ -27,7 +27,7 @@ class HashPreProcessor(object):
         print('[INFO] 转换开始')
         ctx = mx.gpu(1)
         self.model = TripletModelMxnet.deep_conv_lstm()
-        params = os.path.join(ROOT_DIR, "experiments/music_tl_v2/checkpoints", "triplet_loss_model_0_0.9940.params")
+        params = os.path.join(ROOT_DIR, "experiments/music_tl_v2/checkpoints", "triplet_loss_model_92_1.0000.params")
         print('[INFO] 模型: %s' % params)
         self.model.load_params(params, ctx=ctx)
 
@@ -40,48 +40,46 @@ class HashPreProcessor(object):
 
         print('[INFO] X_test1.shape: ' + str(X_test1.shape))
 
-        X_test = X_test1
-        l_list = l_list1
-        n_list = n_list1
+        file_name = 'data_test_v2.npz'
+        data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
+        data_all = np.load(data_path)
+        X_test2 = data_all['f_list']
+        l_list2 = data_all['l_list']
+        n_list2 = data_all['n_list']
+        print('[INFO] X_test2.shape: ' + str(X_test2.shape))
 
-        # file_name = 'data_test_v2.npz'
-        # data_path = os.path.join(ROOT_DIR, 'experiments', file_name)
-        # data_all = np.load(data_path)
-        # X_test2 = data_all['f_list']
-        # l_list2 = data_all['l_list']
-        # n_list2 = data_all['n_list']
-        # print('[INFO] X_test2.shape: ' + str(X_test2.shape))
+        X_test = np.concatenate((X_test1, X_test2), axis=0)
+        l_list = np.concatenate((l_list1, l_list2), axis=0)
+        n_list = np.concatenate((n_list1, n_list2), axis=0)
 
-        # X_test = np.concatenate((X_test1, X_test2), axis=0)
-        # l_list = np.concatenate((l_list1, l_list2), axis=0)
-        # n_list = np.concatenate((n_list1, n_list2), axis=0)
+        print('[INFO] X_test.shape: ' + str(X_test.shape))
 
-        # print('[INFO] X_test.shape: ' + str(X_test.shape))
+        def split_num(name):
+            name = str(name)
+            return len(name.split('.'))  # 长度为2的音频是原始音频
 
-        # def split_num(name):
-        #     name = str(name)
-        #     return len(name.split('.'))  # 长度为2的音频是原始音频
-        #
-        # # 获取原始音频的索引
-        # n_list_r = np.reshape(n_list, (-1, 1))
-        # n_list_num = np.apply_along_axis(split_num, axis=1, arr=n_list_r)
-        # o_indexes = np.where(n_list_num == 2, True, False)  # 原始音频的索引
-        #
-        # n_list = n_list[o_indexes]
-        # l_list = l_list[o_indexes]
-        # X_test = X_test[o_indexes]
+        # 获取原始音频的索引
+        n_list_r = np.reshape(n_list, (-1, 1))
+        n_list_num = np.apply_along_axis(split_num, axis=1, arr=n_list_r)
+        o_indexes = np.where(n_list_num == 2, True, False)  # 原始音频的索引
+
+        n_list = n_list[o_indexes]
+        l_list = l_list[o_indexes]
+        X_test = X_test[o_indexes]
 
         oz_bin_all = np.array([])
         print('[INFO] 转换数量: %s' % n_list.shape[0])
         X_test = np.transpose(X_test, [0, 2, 1])
 
-        for index in range(0, X_test.shape[0], 10000):
-            test = X_test[index: index + 10000]
+        step = 10000
+        for index in range(0, X_test.shape[0], step):
+            test = X_test[index: index + step]
             test = mx.nd.array(test).as_in_context(ctx)
             print('[INFO] 输入结构: %s' % str(test.shape))
             res = self.model(test)
             print('[INFO] 输出结构: %s' % str(res.shape))
             data = res.asnumpy()
+            print('[INFO] data.shape: %s' % str(data.shape))
             oz_arr = np.where(data >= 0.0, 1.0, 0.0).astype(int)
             print oz_arr[0]
             # print np.sum(oz_arr, axis=1)  # 测试分布
