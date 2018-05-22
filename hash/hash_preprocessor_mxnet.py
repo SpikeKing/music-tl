f2a45e68d4d65ff2ca694061c4dd5df6c840f001
wcl1 <wcl1@meitu.com>
2018-05-22 14:18:12 +0800

    update

--- hash/hash_preprocessor_mxnet.py
+++ hash/hash_preprocessor_mxnet.py
@@ -31,7 +31,7 @@ class HashPreProcessor(object):
         #     outputs=mx.sym.load_json(sym_json),
         #     inputs=mx.sym.var('data'))
         self.model = TripletModelMxnet.deep_conv_lstm()
-        params = os.path.join(ROOT_DIR, "experiments/music_tl_v2/checkpoints", "triplet_loss_model_2_0.9807.params")
+        params = os.path.join(ROOT_DIR, "experiments/music_tl_v2/checkpoints", "triplet_loss_model_9_0.9880.params")
         print('[INFO] 模型: %s' % params)
         self.model.load_params(params, ctx=ctx)
 
