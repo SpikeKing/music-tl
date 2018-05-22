15ef2062251ab3222feb2a99689bab7ce926c3be
wcl1 <wcl1@meitu.com>
2018-05-22 15:50:44 +0800

    update

--- models/triplet_model_mxnet.py
+++ models/triplet_model_mxnet.py
@@ -52,11 +52,6 @@ class TripletModelMxnet(ModelBase):
             net_triplet.add(MaxPool1D(pool_size=pool_size))
             net_triplet.add(Dropout(rate=dropout_rate))
 
-            net_triplet.add(Conv1D(channels=128, kernel_size=kernel_size, activation=f_act))
-            net_triplet.add(BatchNorm())
-            net_triplet.add(MaxPool1D(pool_size=pool_size))
-            net_triplet.add(Dropout(rate=dropout_rate))
-
             net_triplet.add(Dense(units=128, activation=f_act))
             net_triplet.add(Dropout(rate=dropout_rate))
 
